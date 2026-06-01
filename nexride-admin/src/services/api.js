4abf6024509a62b99fdb2496bb8ef67d3c885d63
MoneebaSajid <moneebasajid2@gmail.com>
2026-06-01 10:25:42 +0500

    Fix API URL

--- nexride-admin/src/services/api.js
+++ nexride-admin/src/services/api.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
 
-// const API_URL = 'http://192.168.36.77:5000/api';
+const API_URL = process.env.REACT_APP_API_URL 
+  || 'https://nexride-backend.onrender.com/api';
 
 const api = axios.create({
   baseURL: API_URL,
